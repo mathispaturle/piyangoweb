1a78a7c75aff968722b5b6653ec52f73bd61931b
mathisbrickbro <mathis.paturle@brickbro.com>
2025-08-25 12:04:09 +0200

    Added test endpoint

---
+++ src/app/api/wallet/test/route.ts
@@ -0,0 +1,13 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(req: NextRequest) {
+  try {
+    return NextResponse.json({ hello: 'world' }, { status: 200 });
+  } catch (e: any) {
+    console.error(e);
+    return NextResponse.json(
+      { error: 'No se pudo crear el pago' },
+      { status: 500 }
+    );
+  }
+}
