6a203e63dbbd7faa6c2b0e2c4736a58a2c6d46d7
Mathis Paturle <mathis@tribuapp.es>
2025-12-28 20:21:52 +0100

    Changed sender email

--- src/app/api/resend/server_action.js
+++ src/app/api/resend/server_action.js
@@ -8,7 +8,7 @@ export async function createEmail(email_to, email_data) {
     const resend = new Resend(process.env.RESEND);
 
     const data = await resend.emails.send({
-        from: 'onboarding@resend.dev',
+        from: 'soporte@piyango.es',
         to: email_to,
         // bcc: ["mathis@tieneshueco.com", "javi@tieneshueco.com", "info@tieneshueco.com"],
         subject: email_data.subject,
