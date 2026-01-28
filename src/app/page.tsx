5b90d802907fe35c0be1ec8ec5ae41efc41d9f48
Mathis Paturle <mathis@tribuapp.es>
2026-01-28 22:39:09 +0100

    Added flow to recharge when paying for raffle 9

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,8 +13,8 @@ export default function Home() {
     <>
       <Header />
       <Hero />
-      <HowItWorks />
       <FeaturedRaffles />
+      <HowItWorks />
       <FAQ />
       {/* <Download /> */}
       <Footer />
